d8d33eb9b7b2019c9282e5356700adfa8c1808ed
Ximo Lozano <ximo@secture.com>
2024-11-25 16:30:59 +0100

    docs(swagger): configure swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Élevé')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Says "Hello World!".' })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Élevé')
+    .setDescription('The Élevé API description')
+    .setVersion('1.0')
+    .addTag('Élevé')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, documentFactory, {
+    jsonDocumentUrl: 'api/docs.json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
